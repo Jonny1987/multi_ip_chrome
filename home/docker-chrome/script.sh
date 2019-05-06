d6fc6380cd9ee48f2e0e9379becabf13d79a0781
Jonny1987 <jrshannie@yahoo.com>
2019-05-06 09:35:45 +0100

    Shows in the browser which port the usb stick is in. Notifies on browser load if the ip address is the same as the previous launch

--- home/docker-chrome/script.sh
+++ home/docker-chrome/script.sh
@@ -1,2 +1,30 @@
 #!/bin/bash
-sleep 5 && sudo -u chrome google-chrome --no-first-run $1 && /usr/local/bin/script2.sh
+sleep 8
+
+for i in {0..7}
+do
+    sleep 1
+    ip=$(curl ifconfig.me)
+    ip=$([[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; echo ${BASH_REMATCH[0]})
+    if [ ! -z $ip ]
+    then
+        break
+    fi
+done
+
+source /home/chrome/ips
+port=$(udevadm info -p /sys/class/net/eth1 | head -n 1 | cut -d"/" -f 7 | cut -d"." -f 2)
+old_ip=${ips[port]}
+
+bookmarks_txt=$(</home/chrome/.config/google-chrome/Default/Bookmarks)
+bookmarks_txt=${bookmarks_txt/PORT_HERE/$port}
+echo $bookmarks_txt > /home/chrome/.config/google-chrome/Default/Bookmarks
+
+if [ $ip == $old_ip ]
+then
+    sudo -u chrome google-chrome --no-first-run /home/chrome/same_ip.html
+else
+    ips[$port]=$ip
+    declare -p ips > /home/chrome/ips
+    sudo -u chrome google-chrome --no-first-run $1 && /usr/local/bin/script2.sh
+fi
--- usr-local-bin/docker-chrome.sh
+++ usr-local-bin/docker-chrome.sh
@@ -12,7 +12,7 @@ ip netns del $ns
 touch /usr/local/bin/webpage
 source /usr/local/bin/webpage
 echo webpage\:$webpage
-docker run -dit  --rm --privileged --cpuset-cpus 0 -h localhost -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 -v $HOME/Downloads:/home/chrome/Downloads --security-opt seccomp=/usr/local/bin/chrome.json --device /dev/snd --device /dev/dri -v /dev/shm:/dev/shm --net none -v /home/john/Docker/gambling/documents:/home/chrome/documents --name $container docker-chrome4 $webpage;
+docker run -dit --runtime=nvidia --cpuset-cpus 0 -v /usr/local/bin/ips:/home/chrome/ips -h localhost -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 -v $HOME/Downloads:/home/chrome/Downloads --security-opt seccomp=/usr/local/bin/chrome.json --device /dev/snd --device /dev/dri -v /dev/shm:/dev/shm --net none -v /home/john/Docker/gambling/documents:/home/chrome/documents --name $container docker-chrome4 $webpage;
 pid=$(sudo docker inspect --format '{{.State.Pid}}' $container);
 mkdir -p /var/run/netns;
 ln -sf /proc/$pid/ns/net /var/run/netns/$ns;
